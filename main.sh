78ea026e3323cd3a5be3b18498f54c13d5f0475c
Viet Hung Pham <phamvh95@gmail.com>
2021-10-30 23:45:27 +0200

    Annotating the shell script with some longer comments

--- main.sh
+++ main.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/bash
+
+# A small bash script for running all the necessary scripts/programs that are needed for this project. The purpose is to make this project easily reproducible.
+# First we established a temp/ and an output/ folder if they have not existed. If temp existed before, delete all the contents inside that folder
+# We create a virtualenv in order to be able to work with different dependencies so it will not interfere with other projects' dependencies.
+# For being able to harness virtualenv, one needs to pip install it.
+# Installing depencdencies via requirements.txt.
+# Running 4 python scripts:
+# 1.) First the NYT headline scrapers
+# 2.) Reddit meeme scrapers
+# 3.) memes exploratory and label tagging ipynb
+# 4.) a dataset splitter, that is a script that moves the picture in to their appropriate data set folders and generates 3 csv-s
+
 rm -rf temp/*
 mkdir temp/
 mkdir output/
@@ -8,7 +20,7 @@ virtualenv temp/env -p python
 
 pip install -r requirements.txt
 
-python nyt_headline.py > ./temp/logFile_headline.txt
+python nyt_headline.py #> ./temp/logFile_headline.txt
 python meme_scrape.py
 jupyter nbconvert --to notebook --execute memes_exploratory.ipynb --output memes_exploratory.ipynb
 python splitting_dataset.py #>  ./temp/log.txt
