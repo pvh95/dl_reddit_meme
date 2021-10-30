67a3695b9fc29f9de53d4080f5268cb79091048e
Viet Hung Pham <phamvh95@gmail.com>
2021-10-30 23:07:11 +0200

    Usual commenting/uncommenting matter

--- main.sh
+++ main.sh
@@ -11,7 +11,7 @@ pip install -r requirements.txt
 python nyt_headline.py > ./temp/logFile_headline.txt
 python meme_scrape.py
 jupyter nbconvert --to notebook --execute memes_exploratory.ipynb --output memes_exploratory.ipynb
-python splitting_dataset.py >  ./temp/log.txt
+python splitting_dataset.py #>  ./temp/log.txt
 
 deactivate
 
